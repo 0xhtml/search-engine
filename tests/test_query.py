44944366507767b7b76599e187ab8c6ac60fdfdf
0xhtml <34682885+0xhtml@users.noreply.github.com>
2023-10-28 01:56:24 +0200

    Update query parser tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -2,25 +2,25 @@
 
 from typing import Optional
 
+import pytest
+from searchengine.query import parse_query
 
-def test_parse_query(benchmark):
+
+@pytest.mark.parametrize(
+    "query,query_parts,lang",
+    [
+        ("This is a test!", ["This", "is", "a", "test!"], None),
+        ('Th"s "is a" test!', ['Th"s', "is a", "test!"], None),
+        ('This "is a test!', ["This", "is a test!"], None),
+        (' This  "is   a"     test!  ', ["This", "is a", "test!"], None),
+        (':1 "is :2 a" :3 test!', ["is :2 a", "test!"], "3"),
+        ('::1 "is :2 a":: test :', [":1", "is :2 a", ":", "test", ":"], None),
+        ('""  "   " test', ["test"], None)
+    ],
+)
+def test_parse_query(query: str, query_parts: list[str], lang: Optional[str]):
     """Test the parse_query function."""
-    from searchengine.query import parse_query
-
-    def test(inp: str, parts: list[str], lang: Optional[str] = None) -> None:
-        parsed_query = parse_query(inp)
-
-        assert parsed_query.query_parts == parts
-
-        if lang is not None:
-            assert parsed_query.lang == lang
-
-    test("This is a test!", ["This", "is", "a", "test!"])
-    test('Th"s "is a" test!', ['Th"s', "is a", "test!"])
-    test('This "is a test!', ["This", "is a test!"])
-    test(' This  "is   a"     test!  ', ["This", "is a", "test!"])
-    test(':1 "is :2 a" :3 test!', ["is :2 a", "test!"], "3")
-    test('::1 "is :2 a":: test! :', [":1", "is :2 a", ":", "test!", ":"])
-    test('""  "   " test', ["test"])
-
-    benchmark(parse_query, '  Th"is" :de is ::    "  a test" ::right! :en  ')
+    parsed_query = parse_query(query)
+    assert parsed_query.query_parts == query_parts
+    if lang is not None:
+        assert parsed_query.lang == lang
