23ebba66e4e0287a0d253816c3b9b06282e06be1
0xhtml <34682885+0xhtml@users.noreply.github.com>
2024-06-13 01:55:35 +0200

    Highlight longer words before shorter words

--- searchengine/template_filter.py
+++ searchengine/template_filter.py
@@ -11,13 +11,12 @@ from .sha import gen_sha
 def _highlight(string: str, query: ParsedQuery) -> markupsafe.Markup:
     string = markupsafe.escape(string)
 
-    for word in query.words:
-        word = word.lower()
+    for word in sorted(query.words, key=len, reverse=True):
         word_length = len(word)
 
         start = 0
 
-        while (start := string.lower().find(word, start)) > -1:
+        while (start := string.lower().find(word.lower(), start)) > -1:
             bold = (
                 markupsafe.Markup("<b>")
                 + string[start : start + word_length]
@@ -41,8 +40,8 @@ def _pretty_url(url: httpx.URL) -> markupsafe.Markup:
                 url.path,
                 f"?{url.params}" if url.query else "",
                 f"#{url.fragment}" if url.fragment else "",
-            ]
-        )
+            ],
+        ),
     )
 
 
