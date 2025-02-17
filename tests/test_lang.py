356ee05efaf830212b6466fedcea72c804a3fd7e
0xhtml <34682885+0xhtml@users.noreply.github.com>
2025-02-17 16:25:48 +0100

    Add additional tests for detect_lang and is_lang

--- tests/test_lang.py
+++ tests/test_lang.py
@@ -1,7 +1,41 @@
 """Tests for the language handling functions."""
 
 import pytest
-from searchengine.lang import parse_accept_language
+from searchengine.lang import detect_lang, is_lang, parse_accept_language
+
+_LANGUAGE_TEST_CASES = [
+    ("this is english", "en"),
+    ("dies ist deutsch", "de"),
+    ("这是中文", "zh"),
+    ("test", "en"),
+]
+_LANGUAGES = {lang for query, lang in _LANGUAGE_TEST_CASES}
+
+
+@pytest.mark.parametrize(
+    ("query", "languages", "expected"),
+    [
+        (
+            query,
+            ["unk", *(lang for lang in _LANGUAGES if lang != expected), expected],
+            expected,
+        )
+        for query, expected in _LANGUAGE_TEST_CASES
+    ]
+    + [(query, ["unk"], "unk") for query, expected in _LANGUAGE_TEST_CASES],
+)
+def test_detect_lang(query: str, languages: list[str], expected: str) -> None:
+    """Test the detect_lang function."""
+    assert detect_lang(query, languages) == expected
+
+
+@pytest.mark.parametrize(("query", "expected"), _LANGUAGE_TEST_CASES)
+def test_is_lang(query: str, expected: str) -> None:
+    """Test the is_lang function."""
+    assert is_lang(query, expected) > 0.2
+    for lang in _LANGUAGES:
+        if lang != expected:
+            assert is_lang(query, lang) < 0.02
 
 
 @pytest.mark.parametrize(
