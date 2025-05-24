8ef63d5c947d8d0bfebdc88eb95c19c1ce41cfec
0xhtml <34682885+0xhtml@users.noreply.github.com>
2025-05-24 17:44:30 +0200

    Update tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,34 +1,62 @@
 """Tests for the query parsing and string convertion."""
 
-from typing import Optional
-
 import pytest
-from searchengine.query import parse_query
+from searchengine.query import ParsedQuery, parse_query
 
 
 @pytest.mark.parametrize(
-    ("query", "words", "lang", "site"),
+    ("query", "parsed_query"),
     [
-        ("This is a test!", ["This", "is", "a", "test!"], None, None),
-        ('Th"s "is a" test!', ['Th"s', "is a", "test!"], None, None),
-        ('This "is a test!', ["This", "is a test!"], None, None),
-        (' This  "is   a"     test!  ', ["This", "is   a", "test!"], None, None),
-        (':1 "is :2 a" :de test!', [":1", "is :2 a", "test!"], "de", None),
-        ('::1 "is :2 a":: test :', ["::1", "is :2 a", "::", "test", ":"], None, None),
-        ('""  "   " test', ['""', "   ", "test"], None, None),
-        (": :: :::", [":", "::", ":::"], None, None),
-        ("site:", ["site:"], None, None),
-        ('":de" :en', [":de"], "en", None),
-        ('"lang:de" lang:en', ["lang:de"], "en", None),
-        ('"site:1" site:2', ["site:1"], None, "2"),
+        (
+            "This is a test!",
+            ParsedQuery(("This", "is", "a", "test!"), None, None),
+        ),
+        (
+            'Th"s "is a" test!',
+            ParsedQuery(('Th"s', "is a", "test!"), None, None),
+        ),
+        (
+            'This "is a test!',
+            ParsedQuery(("This", "is a test!"), None, None),
+        ),
+        (
+            ' This  "is   a"     test!  ',
+            ParsedQuery(("This", "is   a", "test!"), None, None),
+        ),
+        (
+            ':1 "is :2 a" :de test!',
+            ParsedQuery((":1", "is :2 a", "test!"), "de", None),
+        ),
+        (
+            '::1 "is :2 a":: test :',
+            ParsedQuery(("::1", "is :2 a", "::", "test", ":"), None, None),
+        ),
+        (
+            '""  "   " test',
+            ParsedQuery(('""', "   ", "test"), None, None),
+        ),
+        (
+            ": :: :::",
+            ParsedQuery((":", "::", ":::"), None, None),
+        ),
+        (
+            "site:",
+            ParsedQuery(("site:",), None, None),
+        ),
+        (
+            '":de" :en',
+            ParsedQuery((":de",), "en", None),
+        ),
+        (
+            '"lang:de" lang:en',
+            ParsedQuery(("lang:de",), "en", None),
+        ),
+        (
+            '"site:1" site:2',
+            ParsedQuery(("site:1",), None, "2"),
+        ),
     ],
 )
-def test_parse_query(
-    query: str, words: list[str], lang: Optional[str], site: Optional[str]
-) -> None:
+def test_parse_query(query: str, parsed_query: ParsedQuery) -> None:
     """Test the parse_query function."""
-    parsed_query = parse_query(query)
-    assert parsed_query.words == words
-    if lang is not None:
-        assert parsed_query.lang == lang
-    assert parsed_query.site == site
+    assert parse_query(query) == parsed_query
